03ed43efe55b1cb207afb7f4c129004c87c177f1
Durbadol Goswami <goswamidurbadol@gmail.com>
2024-11-08 09:19:59 +0600

    Updated settings.py file

--- FlowerWorld/settings.py
+++ FlowerWorld/settings.py
@@ -69,11 +69,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://127.0.0.1:5500",
-#     "https://flower-world-frontend-new.vercel.app"
-# ]
-# CSRF_TRUSTED_ORIGINS = ['https://flowerworld.onrender.com','https://flower-world-frontend-new.vercel.app','https://*.127.0.0.1',]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    #"https://flower-world-frontend-new.vercel.app"
+]
+CSRF_TRUSTED_ORIGINS = ['https://flowerworld-modified.onrender.com','https://*.127.0.0.1',]
 
 ROOT_URLCONF = 'FlowerWorld.urls'
 
