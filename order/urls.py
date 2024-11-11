2da611fb439015e51e9ea7429523bfaac62792dc
Durbadol Goswami <goswamidurbadol@gmail.com>
2024-11-11 11:22:13 +0600

    changes occured

--- order/urls.py
+++ order/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import OrderViewSet
-
-# router = DefaultRouter()
-# router.register(r'orders', OrderViewSet, basename='order')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet, CartViewSet, CartItemViewSet
