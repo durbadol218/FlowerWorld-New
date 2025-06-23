684eef042d9e08773c1c5eff2e6cab4838146426
Durbadol Goswami <goswamidurbadol@gmail.com>
2025-06-23 11:34:11 +0600

    updated activation link

--- user/views.py
+++ user/views.py
@@ -68,7 +68,7 @@ class UserRegistrationApiView(APIView):
                 user = serializer.save()
                 token = default_token_generator.make_token(user)
                 uid = urlsafe_base64_encode(force_bytes(user.pk))
-                confirm_link = f"https://flower-world.vercel.app/user/activate/{uid}/{token}"
+                confirm_link = f"https://flowerworld-api.vercel.app/user/activate/{uid}/{token}"
                 print(confirm_link)
                 email_subject = "Confirmation Email for Activate Account"
                 email_body = render_to_string('confirm_email.html', {'confirm_link': confirm_link})
